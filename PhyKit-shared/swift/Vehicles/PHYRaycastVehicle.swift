cbe2e1ccdeae795844a75d4dbd0b5c4eca357afd
adityajs12321 <77228612+adityajs12321@users.noreply.github.com>
2023-06-11 20:00:12 +0530

    Fix Vehicle 3.0

--- PhyKit-shared/swift/Vehicles/PHYRaycastVehicle.swift
+++ PhyKit-shared/swift/Vehicles/PHYRaycastVehicle.swift
@@ -9,6 +9,6 @@ import Foundation
 
 public class PHYRaycastVehicle: CPHYRaycastVehicle {
     public init(chassisShape: PHYCollisionShape, world: PHYWorld, mass: Float, wheelWidth: Float, wheelRadius: Float, maxEngineForce: Float, maxBreakingForce: Float, wheelFriction: Float, suspensionStiffness: Float, suspensionDamping: Float, suspensionCompression: Float, suspensionRestLength: Float, rollInfluence: Float) {
-        super.init(chassisShape: chassisShape as! CPHYCollisionShape, world: world, mass: mass, wheelWidth: wheelWidth, wheelRadius: wheelRadius, maxEngineForce: maxEngineForce, maxBreakingForce: maxBreakingForce, wheelFriction: wheelFriction, suspensionStiffness: suspensionStiffness, suspensionDamping: suspensionDamping, suspensionCompression: suspensionCompression, suspensionRestLength: suspensionRestLength, rollInfluence: rollInfluence)
+        super.init(chassisShape: chassisShape.internalShape, world: world, mass: mass, wheelWidth: wheelWidth, wheelRadius: wheelRadius, maxEngineForce: maxEngineForce, maxBreakingForce: maxBreakingForce, wheelFriction: wheelFriction, suspensionStiffness: suspensionStiffness, suspensionDamping: suspensionDamping, suspensionCompression: suspensionCompression, suspensionRestLength: suspensionRestLength, rollInfluence: rollInfluence)
     }
 }
