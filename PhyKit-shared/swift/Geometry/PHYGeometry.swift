1a73f523cb96919c596e93fdd9c3a4046234b1c5
adityajs12321 <77228612+adityajs12321@users.noreply.github.com>
2023-07-19 12:48:36 +0530

    Fix bugs

--- PhyKit-shared/swift/Geometry/PHYGeometry.swift
+++ PhyKit-shared/swift/Geometry/PHYGeometry.swift
@@ -157,9 +157,3 @@ public class PHYGeometry {
     }
     
 }
-
-extension Collection where Indices.Iterator.Element == Index {
-   public subscript(safe index: Index) -> Iterator.Element? {
-     return (startIndex <= index && index < endIndex) ? self[index] : nil
-   }
-}
