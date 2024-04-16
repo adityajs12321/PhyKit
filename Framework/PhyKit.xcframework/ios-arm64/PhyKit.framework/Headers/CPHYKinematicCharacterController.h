acfa1abb330555be392d09562901f861dc1efd37
adityajs12321 <77228612+adityajs12321@users.noreply.github.com>
2024-04-16 22:02:21 +0530

    new 5

---
+++ Framework/PhyKit.xcframework/ios-arm64/PhyKit.framework/Headers/CPHYKinematicCharacterController.h
@@ -0,0 +1,22 @@
+//
+//  CPHYKinematicCharacterController.h
+//  PhyKit
+//
+//  Created by Aditya JS on 09/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+struct PHYVector3;
+@class CPHYRigidBody;
+@class CPHYWorld;
+
+@interface CPHYKinematicCharacterController : NSObject
+
+-(instancetype)initWithEntity: (CPHYRigidBody*)rigidBody shapeRadius:(float)radius shapeHeight:(float)height stepHeight: (float)stepHeight world:(CPHYWorld*)world;
+
+@end
+
+NS_ASSUME_NONNULL_END
